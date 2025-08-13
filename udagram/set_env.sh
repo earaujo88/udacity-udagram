0068cf339582ace6758f50d136a9ea3f687fdc79
Eduardo De Araujo <earaujo8@its.jnj.com>
2025-08-12 21:41:39 -0300

    starting ci/cd

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=database-1.cv0jrf4ojxwt.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::final-project-079455686433
+export AWS_BUCKET=final-project-079455686433
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
